d846186cd27aa91a0a7f265279b62c73ce0cc5b2
Behshad <behshad.babai@gmail.com>
2021-11-11 19:14:55 -0800

    writing a test case for the announceDawn function

--- index-test.js
+++ index-test.js
@@ -1,10 +1,15 @@
 const assert = require('assert');
-const rooster = require('../index.js');
+const rooster = require('./index.js');
 
 describe('Rooster', () => {
     describe('.announceDawn', () => {
         it('returns a rooster call', () => {
-
+            // Define expected output
+            const expected = 'cock-a-doodle-doo!';
+            // Evaluating actual result
+            const actual = rooster.announceDawn();
+            // Assert Verification
+            assert.strictEqual(actual, expected);
         });
     });
 });
