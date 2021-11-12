b8a1c6b6b6fc84add6df8dff95d5deec10aff51f
Behshad <behshad.babai@gmail.com>
2021-11-11 18:52:09 -0800

    structuring the test suite

---
+++ index-test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const rooster = require('../index.js');
+
+describe('Rooster', () => {
+    describe('.announceDawn', () => {
+        it('returns a rooster call', () => {
+
+        });
+    });
+});
